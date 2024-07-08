f95372f7ad7cecdb9656bc40ffaa3159e10d4ab2
aman <kushwaha@gmail.com>
2024-07-08 15:02:51 +0530

    add socket logic

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+const io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log(`New Connection : ${socket.id}`);
+
+  //receive event
+
+  socket.on("comment", (data) => {
+    data.time = Date();
+    socket.broadcast.emit("comment", data);
+  });
 });
